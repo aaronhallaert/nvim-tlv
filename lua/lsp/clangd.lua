156bf3e507d68690dff242d17ebcfac78cbc7a14
Aaron Hallaert <hallaertaaron@gmail.com>
2024-09-04 17:38:23 +0200

    feat: add entrypoint for plixus_apps development

---
+++ lua/lsp/clangd.lua
@@ -0,0 +1,24 @@
+local nvim_lsp = require 'lspconfig'
+return {
+  root_dir = nvim_lsp.util.root_pattern 'CMakeLists.txt',
+  single_file_support = true,
+  capabilities = {
+    textDocument = {
+      completion = {
+        editsNearCursor = true,
+      },
+    },
+    offsetEncoding = { 'utf-16' },
+  },
+  on_new_config = function(new_config)
+    local status, cmake = pcall(require, 'cmake-tools')
+    if status then
+      cmake.clangd_on_new_config(new_config)
+    end
+  end,
+  cmd = {
+    '/usr/bin/clangd',
+    '--clang-tidy',
+    '--query-driver=' .. require('televic').build_scripts_path .. '/toolchains/' .. require('televic').toolchainfile .. '/bin/i686-linux-g++',
+  },
+}
---
+++ lua/televic/init.lua
@@ -0,0 +1,5 @@
+return {
+  plixus_apps_build_dir = 'build_pc',
+  build_scripts_path = vim.fn.expand '$HOME/Developer/televic/build_scripts', -- without terminating slash
+  toolchainfile = 'televic_pc_sdk_2023.02.7',
+}
